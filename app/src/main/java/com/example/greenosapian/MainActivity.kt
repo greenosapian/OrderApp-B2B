0a9196a3f627c9c71f6b766c63b98a885a377a3b
godfather_rana <mail2ameetrana@gmail.com>
2021-01-18 19:55:35 +0530

    just a comment

--- app/src/main/java/com/example/greenosapian/MainActivity.kt
+++ app/src/main/java/com/example/greenosapian/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //HELLO RANA BHAI GIT TEST SUCCESSFUL
+        // hello again..kaise hosadsadasd
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
