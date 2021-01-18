6ca84bdc106d6fe204e71dfbdced7133845b4e4d
shubamuzumaki <shubamuzumaki@gmail.com>
2021-01-18 20:13:18 +0530

    Git collab testing_3

--- app/src/main/java/com/example/greenosapian/MainActivity.kt
+++ app/src/main/java/com/example/greenosapian/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         //HELLO RANA BHAI GIT TEST SUCCESSFUL
         // hello again..kaise hosadsadasd
         // hello from another world
+        // commit from cart
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
