ef9003e9daf81f4480e6f806951604674198ffd9
godfather_rana <mail2ameetrana@gmail.com>
2021-01-20 15:46:03 +0530

    dialog_order_placed

--- app/src/main/java/com/example/greenosapian/MainActivity.kt
+++ app/src/main/java/com/example/greenosapian/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.greenosapian
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import kotlinx.android.synthetic.main.dialog_order_placed.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //HELLO RANA BHAI GIT TEST SUCCESSFUL
         // hello again..kaise hosadsadasd
